cfedd44c7796181d87098e22f2b6bcf860e8cd6c
SpeedCrash100 <SpeedCrash100@yandex.ru>
2023-07-11 17:16:09 +0300

    Fix: missing early return in /shorten

--- internal/routes/shorten.go
+++ internal/routes/shorten.go
@@ -19,6 +19,7 @@ func Shorten(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadGateway, gin.H{
 			"message": err.Error(),
 		})
+		return
 	}
 
 	url_regexp := "^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
